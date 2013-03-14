078a566cfd992fe78984f9a3e3d5ee2b8da54dc3
Nick Johnson <arachnid@notdot.net>
2013-03-14 12:13:36 +0000

    Added LCD font and IR control

--- firmware/main.c
+++ firmware/main.c
@@ -1,15 +1,18 @@
 #define F_CPU 8000000UL  // 8 MHz
 
 #include <inttypes.h>
+#include <avr/eeprom.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
+#include <stdio.h>
 
 #include "glcdfont.h"
 
 FUSES = {
+	// Internal 8mhz oscillator; short startup; enable ISP programming.
 	.low = FUSE_SUT0 & FUSE_CKSEL3 & FUSE_CKSEL1 & FUSE_CKSEL0,
 	.high = FUSE_SPIEN,
 };
@@ -17,9 +20,42 @@ FUSES = {
 #define PWM_ON() (TCCR1B |= _BV(CS11))
 #define PWM_OFF() (TCCR1B &= ~_BV(CS11))
 
-char display[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-int row = 0;
+#define MAX_MESSAGE_LENGTH 248
 
+#define IR_MESSAGE_LENGTH 14
+
+#define MODE_MARQUEE 0
+#define MODE_EDIT 1
+
+typedef union {
+	uint16_t data;
+	struct {
+		unsigned int command : 11;
+		unsigned int toggle : 1;
+		unsigned int field : 1;
+		unsigned int empty : 3;
+	} message;
+} ir_message_t;
+
+typedef struct {
+	uint8_t delay;
+	uint8_t spacing;
+} config_t;
+
+typedef void (*mode_func)(void);
+
+config_t stored_config EEMEM = { .delay = 15, .spacing = 1 };
+char message[MAX_MESSAGE_LENGTH] EEMEM = "www.arachnidlabs.com ";
+
+config_t config;
+uint8_t display[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t row = 0;
+uint8_t last_ir_level = 1;
+uint8_t ir_counter = 0;
+uint8_t ir_bit_counter = 0;
+ir_message_t current_message;
+mode_func mode;
+ 
 const char row_pins[] = {
 	_BV(PD6),
 	_BV(PD5),
@@ -31,13 +67,71 @@ const char row_pins[] = {
 	_BV(PA0)
 };
 
+#define PORTD_ROWS _BV(PD6) | _BV(PD5) | _BV(PD4) | _BV(PD3) | _BV(PD2) | _BV(PD1)
+#define PORTA_ROWS _BV(PA1) | _BV(PA0)
+
 #define ENABLE_ROW(row) if(row < 6) PORTD &= ~row_pins[row]; else PORTA &= ~row_pins[row]
+
+#define COMMAND_NONE 	0xFFFF
+#define COMMAND_STANDBY 0x000C
+#define COMMAND_UP		0x0021
+#define COMMAND_DOWN	0x0020
+#define COMMAND_LEFT	0x0010
+#define COMMAND_RIGHT	0x0011
+#define COMMAND_MENU	0x000B
+
+int8_t previous_toggle = -1;
+
+uint16_t get_message() {
+	if(ir_bit_counter < IR_MESSAGE_LENGTH)
+		return COMMAND_NONE;
+	return current_message.message.command;
+}
+
+uint8_t message_is_repeat() {
+	return previous_toggle == current_message.message.toggle;
+}
+
+void ir_clear_buffer(void) {
+	previous_toggle = current_message.message.toggle;
+	ir_bit_counter = 0;
+}
+
+void ir_receive(void) {
+	if(ir_bit_counter == IR_MESSAGE_LENGTH)
+		return;
+
+	uint8_t ir_level = PIND & _BV(PD0);
+	if(ir_level == last_ir_level) {
+		// Increment the time intervals since last bit flip
+		ir_counter++;
+		if(ir_counter > 20)
+			ir_clear_buffer(); // Long pause, reset
+	} else {
+		if(ir_counter > 8) {
+			// Direction of transition indicates bit value
+			// space to mark (ir_level=0) is a 1, mark to space is 0.
+			current_message.data <<= 1;
+			if(!ir_level)
+				current_message.data |= 1;
+			ir_bit_counter++;
+			ir_counter = 0;
+		} else {
+			// This transition is between bits, ignore it
+		}
+		
+		last_ir_level = ir_level;
+	}
+}
 
 ISR(TIMER1_COMPA_vect) {
 	// Turn off the old row
-	PORTD = 0xff;
-	PORTA = 0xff;
+	PORTD |= PORTD_ROWS;
+	PORTA |= PORTA_ROWS;
 	
+	// Poll the IR receiver
+	ir_receive();
+
 	// Set the column data
 	row = (row + 1) & 0x7;
 	PORTB = display[row];
@@ -47,70 +141,155 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 void ioinit(void) {
-	OCR1A = 1250; // 8 megahertz / 8 / 1250 = 800 hertz
+	OCR1A = 125; // 8 megahertz / 8 / 125 = 8KHz
 	TIMSK |= _BV(OCIE1A); // Interrupt on counter reset
-	TCCR1B = _BV(WGM12) | _BV(CS11); // CTC(OCR1A), /8 prescaler
-	
+	TCCR1B = _BV(WGM12); // CTC(OCR1A), /8 prescaler
+
+	// PORTB is all output for columns
 	DDRB = 0xff;
-	DDRD = _BV(PD1) | _BV(PD2) | _BV(PD3) | _BV(PD4) | _BV(PD5) | _BV(PD6);
-	DDRA = _BV(PA0) | _BV(PA1);
+
+	// PORTD is output for rows except PD0 which is the IR input
+	DDRD = PORTD_ROWS;
+	// Enable pullup on IR receiver pin
+	PORTD |= _BV(PD0);
+
+	// PORTA is output for rows
+	DDRA = PORTA_ROWS;
 	
 	sei();
 }
 
-char sense_columns() {
-	char ret;
-
-	PWM_OFF();
-
-	// Make the columns output low
-	PORTB = 0;
-
-	// Bring all the rows high
-	PORTD = 0xff;
-	PORTA = 0xff;
-	
-	_delay_us(100);
-
-	// Now make the columns inputs
-	DDRB = 0;
-		
-	_delay_ms(6);
-	
-	// Now read the columns
-	ret = PINB;
-
-	// Make the columns outputs again
-	DDRB = 0xff;
-
-	PWM_ON();
-	
-	return ret;
+uint8_t read_font_column(char character, uint8_t column) {
+	return pgm_read_byte(font + (character * 5) + column);
 }
 
-int main(void) {
-	ioinit();
-	
-	for(;;) {
-		/*int i;
-		for(i = 7; i >= 1; i--)
-			display[i] = display[i-1];
-		display[0] += 1;
-		display[0] = sense_columns();
-		_delay_ms(1000);*/
-		int i, j, k;
-		for(i = 0; i < 256; i++) {
-			for(j = 0; j < 7; j++) {
-				for(k = 7; k > 0; k--)
-					display[k] = display[k - 1];
-				if(j >= 5) {
-					display[0] = 0;
-				} else { 
-					display[0] = pgm_read_byte(font+(i*5)+j);
+void marquee(void);
+void edit(void);
+
+void marquee(void) {
+	char *msgptr = message;
+	while(mode == marquee) {
+		char current = eeprom_read_byte((uint8_t*)msgptr++);
+		if(current == '\0') { 
+			msgptr = message;
+			continue;
+		}
+
+		int j, k;
+		for(j = 0; j < 5 + config.spacing; j++) {
+			// Shift everything else left
+			for(k = 7; k > 0; k--)
+				display[k] = display[k - 1];
+			
+			if(j >= 5) {
+				// Empty columns at the end
+				display[0] = 0;
+			} else {
+				// Display the next column of the current letter 
+				display[0] = read_font_column(current, j);
+			}
+
+			for(k = 0; k < config.delay && mode == marquee; k++) {
+				_delay_ms(10);
+				uint16_t cmd = get_message();
+				if(cmd == COMMAND_NONE) continue;
+				
+				switch(cmd) {
+				case COMMAND_MENU:
+					if(!message_is_repeat())
+						mode = edit;
+					break;
 				}
-				_delay_ms(150);
-			}		
+				ir_clear_buffer();
+			}
 		}
 	}
 }
 
+void edit() {
+	uint8_t repeats = 0;
+	int idx = 0;
+	char current;
+
+	void write_dirty(void) {
+		eeprom_update_byte((uint8_t*)&message[idx], current);
+	}
+	
+	void show_current(void) {
+		int i;
+		for(i = 0; i < 5; i++)
+			display[5 - i] = read_font_column(current, i);
+	}
+	
+	void read_current(void) {
+		current = eeprom_read_byte((uint8_t*)&message[idx]);
+		show_current();
+	}
+	
+	display[0] = 0;
+	display[6] = 0;
+	display[7] = 0;
+	
+	read_current();
+
+	while(mode == edit) {
+		uint16_t cmd = get_message();
+		if(cmd == COMMAND_NONE) {
+			repeats = 0;
+			continue;
+		}
+
+		switch(get_message()) {
+		case COMMAND_LEFT:
+			if(idx > 0) {
+				write_dirty();
+				idx--;
+				read_current();
+			}
+			break;
+		case COMMAND_RIGHT:
+			if(idx < MAX_MESSAGE_LENGTH && current != '\0') {
+				write_dirty();
+				idx++;
+				read_current();
+			}
+			break;
+		case COMMAND_UP:
+			current++;
+			show_current();
+			break;
+		case COMMAND_DOWN:
+			current--;
+			show_current();
+			break;
+		case COMMAND_MENU:
+			if(!message_is_repeat()) {
+				write_dirty();
+				mode = marquee;
+			}
+			break;
+		default:
+			continue;
+		}
+
+		if(repeats == 0)
+			_delay_ms(750);
+		ir_clear_buffer();
+		_delay_ms(250);
+		repeats++;
+	}
+}
+
+int main(void) {
+	eeprom_read_block(&config, &stored_config, sizeof(config_t));
+	
+	mode = marquee;
+	
+	ioinit();
+
+	PWM_ON();
+	
+	for(;;) {
+		mode();
+	}
+}
